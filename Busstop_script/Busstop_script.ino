9e6ef1f5e33cb12a7e84f9eae78d320f6f0b3ea6
TBroe <44840949+TBroe@users.noreply.github.com>
2022-03-11 23:33:16 +0100

    update busstop_script

--- Busstop_script/Busstop_script.ino
+++ Busstop_script/Busstop_script.ino
@@ -13,6 +13,11 @@ last revision November 2015
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <ArduinoJson.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+StaticJsonDocument<192> doc;
 
 #include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
@@ -59,11 +64,22 @@ void setup() {
     // wait 10 seconds for connection:
     delay(10000);
   }
-  Serial.println("Connected to WiFi");
-  printWiFiStatus();
+  Serial.println("Connected to WiFi");  printWiFiStatus();
 
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("tram");
+  lcd.setCursor(0,1);
+  lcd.print("8");
+  lcd.setCursor(5,0);
+  lcd.print("time");
+  lcd.setCursor(11,0);
+  lcd.print("delay");
+  
 
 }
 
@@ -72,31 +88,40 @@ void loop() {
   if (client.connect(server, 443)) {
     Serial.println("connected to server");
     // Make a HTTP request:
-    client.println("GET /v1/connections?from=Klusplatz&to=Hinterbergstrasse&limit=2&fields[]=connections/from/departure HTTP/1.1");
+    client.println("GET /v1/connections?from=Klusplatz&to=Bellevue&limit=1&fields[]=connections/from/departure HTTP/1.1");
     client.println("Host: www.transport.opendata.ch");
     client.println("Connection: close");
     client.println();
     delay(5000);
-
+       
     
   }
   // if there are incoming bytes available
   // from the server, read them and print them:
   while (client.available()) {
-    char c = client.read();
-    String res = String(c);
-    Serial.print(res);
-    // Deserialize the JSON document
-  
-    //DeserializationError error = deserializeJson(doc, res, DeserializationOption::Filter(filter));
-    // Test if parsing succeeds.
-//    if (error) {
-//    Serial.print(F("deserializeJson() failed: "));
-//    Serial.println(error.f_str());
-//    return;
-//  }
-//    // Print the result
-//    serializeJsonPretty(doc, Serial);
+    String res = client.readStringUntil('\n');
+    if (res.startsWith("{") ) {
+      //Serial.print(res);
+      
+    DeserializationError error = deserializeJson(doc, res);
+    
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+    const char* departure_ = doc["connections"][0]["from"]["departure"];
+    String departure = String(departure_);
+    String time_only = departure.substring(11,16);
+    String delay_ = departure.substring(19,22);
+    Serial.print(time_only);
+    lcd.setCursor(5,1);   //Set cursor to character 2 on line 0
+    lcd.print(time_only);
+    lcd.setCursor(11,1);   //Set cursor to character 2 on line 0
+    lcd.print(delay_);
+    }
+    }
+
 
   // if the server's disconnected, stop the client:
   if (!client.connected()) {
@@ -111,7 +136,7 @@ void loop() {
     // Deserialize the document
     //StaticJsonDocument<400> doc;
   }
-}
+  delay(60000);
 }
 
 void printWiFiStatus() {
